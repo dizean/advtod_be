ad1873d26e205e61cb5da3acce09061c6034e339
ean <torrescharles0307@gmail.com>
2024-05-28 22:43:23 +0800

    haha

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import TodoRouter from './routes/Todo';
 import OwnerRoute from './routes/Owner';
 import Accountroute from './routes/Account';
 import Listroute from './routes/List';
+import ItemsRoute from './routes/Items';
 
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -18,6 +19,7 @@ app.use('/account',Accountroute); //route for owners
 app.use('/todo',TodoRouter); // route for todo items
 app.use('/owner',OwnerRoute); //route for owners
 app.use('/list',Listroute);//route for list
+app.use('/items',ItemsRoute);//route for list
 app.listen(PORT, () => {
   console.log(`Listening on port http://localhost:${PORT}`);
 });
--- routes/List.ts
+++ routes/List.ts
@@ -4,7 +4,8 @@ import ListControl from "../controllers/List";
 
 
 router.route("/create").post(ListControl.Create); // create
-// router.route("/display").get(ListControl.Get); // get
+router.route("/delete/:id").delete(ListControl.Delete); // get
+router.route("/update/:id").put(ListControl.Update); // get
 const Listroute = router;
 
 export default Listroute;
